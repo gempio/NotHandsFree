e8a1010f7f4657adacb468215946f9a088eedd04
Harry Reeder <harry@harryreeder.co.uk>
2015-03-14 19:00:13 +0000

    Updates

--- NotHandsFree/views.py
+++ NotHandsFree/views.py
@@ -1,6 +1,15 @@
-from NotHandsFree import app, backend, sockets
+from NotHandsFree import app, backend, sockets, redis
 
-from flask import render_template, jsonify
+from flask import render_template, jsonify, request
+
+import gevent
+
+import logging
+from logging import StreamHandler
+
+file_handler = StreamHandler()
+app.logger.setLevel(logging.DEBUG)  # set the desired logging level here
+app.logger.addHandler(file_handler)
 
 @app.route("/")
 def home():
@@ -8,7 +17,15 @@ def home():
 
 @app.route("/input", methods=['POST'])
 def recv_input():
-    return jsonify(ok="ok")
+    json = request.get_json(force=True)
+    for key in json:
+        val = json[key]
+        del json[key]
+        json[str(key)] = str(val)
+        
+    app.logger.debug(json)
+    redis.publish(app.config['REDIS_CHAN'], json)
+    return jsonify(status="OK")
 
 @sockets.route('/ws')
 def ws_receive(ws):

d69fa17bc242bbb832ef1219205c44d149315048
Harry Reeder <harry@harryreeder.co.uk>
2015-03-15 02:47:21 +0000

    Add flask-heroku

--- NotHandsFree/__init__.py
+++ NotHandsFree/__init__.py
@@ -6,9 +6,17 @@ from flask.ext.sqlalchemy import SQLAlchemy
 from raven.contrib.flask import Sentry
 
 app = Flask(__name__)
+
+heroku = Heroku()
+heroku.init_app(app)
+
 app.config['SQLALCHEMY_DATABASE_URI'] = os.environ['DATABASE_URL']
 app.config['REDIS_URL'] = os.environ['REDISTOGO_URL']
 app.config['REDIS_CHAN'] = "nothandsfree"
+
+app.config['TWILIO_ACCOUNT_SID'] = os.environ['TWILIO_ACCOUNT_SID']
+app.config['TWILIO_AUTH_TOKEN'] = os.environ['TWILIO_AUTH_TOKEN']
+app.config['TWILIO_CALLER_ID'] = os.environ['TWILIO_CALLER_ID']
 
 sentry = Sentry(app)
 redis = redis.from_url(app.config['REDIS_URL'])

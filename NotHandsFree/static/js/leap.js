6c51ee8c15d9e616802f60cd6bc3fac61d62c2f1
Gempio <mmusialek1993@gmail.com>
2015-03-15 03:47:05 +0000

    HelloWorld2

--- NotHandsFree/static/js/leap.js
+++ NotHandsFree/static/js/leap.js
@@ -1,1 +1,10 @@
 console.log("Hello World");
+
+var controller = Leap.loop({enableGestures:true}, function(frame){
+    var currentFrame = frame;
+    var previousFrame = controller.frame(1);
+    var tenFramesBack = controller.frame(10);
+	frame.hands.forEach(function(hand,index) {
+			console.log(hand.roll());
+	});
+});
